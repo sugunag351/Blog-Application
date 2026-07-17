cb349f02cc84532fa52616d49af7ebb7ec2ffa5c
sugunag351 <168339766+sugunag351@users.noreply.github.com>
2026-07-17 11:13:37 +0530

    Delete src directory

--- src/main/java/org/mountblue/BlogApplication/Repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package org.mountblue.BlogApplication.Repository;
-
-import org.mountblue.BlogApplication.Entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
-
